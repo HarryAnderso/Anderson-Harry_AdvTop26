13f2ea45576d22c65a111588d3fe6f798db3e9f5
andeharr <andeharr@OA-S139-03>
2026-09-17 11:56:11 -0400

    work

---
+++ Assets/Scripts/GridSpawner.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class GridSpawner : MonoBehaviour
+{
+     public GameObject TileFab;
+    [Header("Grid Size Input")]
+    public int Width = 10;
+    public int length = 10;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        for(int i = 0; i < length; i++)
+        {
+            for (int j = 0; j < Width; j++)
+            {
+                Instantiate(TileFab, new Vector3(j, 0, i), Quaternion.identity);
+              Instantiate
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
